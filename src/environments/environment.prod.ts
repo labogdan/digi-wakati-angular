27b6c8ad56ff2a7513f423786427d291201f3b2b
Andy Bogdan <abogdan@MacBook-Pro.local>
2020-10-26 12:44:08 -0400

    prod environment url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: false,
+  hybrisServerUrl: 'https://api.cxur-kaocorpor1-s3-public.model-t.cc.commerce.ondemand.com/kaowebservices/v2'
 };
